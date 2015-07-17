a3214294b387e8f95b4278548a78f8d0637abd24
Михаил <beatbox787@gmail.com>
2015-07-17 14:13:07 +0600

    no message

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){	
+    function resize(){
+       if( typeof( window.innerWidth ) == 'number' ) {
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || 
+        document.documentElement.clientHeight ) ) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+    }
+    $(window).resize(resize);
+    resize();
+    
+	// var myPlace = new google.maps.LatLng(55.754407, 37.625151);
+ //    var myOptions = {
+ //        zoom: 16,
+ //        center: myPlace,
+ //        mapTypeId: google.maps.MapTypeId.ROADMAP,
+ //        disableDefaultUI: true,
+ //        scrollwheel: false,
+ //        zoomControl: true
+ //    }
+ //    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
+
+ //    var marker = new google.maps.Marker({
+	//     position: myPlace,
+	//     map: map,
+	//     title: "Ярмарка вакансий и стажировок"
+	// });
+
+    //  var options = {
+    //     $AutoPlay: true,                                
+    //     $SlideDuration: 500,                            
+
+    //     $BulletNavigatorOptions: {                      
+    //         $Class: $JssorBulletNavigator$,             
+    //         $ChanceToShow: 2,                           
+    //         $AutoCenter: 1,                            
+    //         $Steps: 1,                                  
+    //         $Lanes: 1,                                  
+    //         $SpacingX: 10,                              
+    //         $SpacingY: 10,                              
+    //         $Orientation: 1                             
+    //     }
+    // };
+
+    // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+});
